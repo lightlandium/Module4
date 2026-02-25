cfe250516d206dc2295312eb007fc9c8ae9c60ba
lightlandium <popova_sveta_03@mail.ru>
2026-02-25 15:27:34 +0500

    19 урок

---
+++ Selection Sort.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    """Сортирует список методом выбора."""
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Пример использования:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+selection_sort(my_list)
+print("Отсортированный список:", my_list)
