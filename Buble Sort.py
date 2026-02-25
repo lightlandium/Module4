182693a81174afffa5d173e78b3d304ab0d69a3b
lightlandium <popova_sveta_03@mail.ru>
2026-02-25 15:21:15 +0500

    17 урок

---
+++ Buble Sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    """Сортирует список методом пузырька (сортировка пузырьком)."""
+    n = len(arr)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+# Пример использования:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(my_list)
+print("Отсортированный список:", my_list)
